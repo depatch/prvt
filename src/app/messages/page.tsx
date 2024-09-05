cf30f7896d4c6200e4f51c9f175dd2712a98d2b6
resatu <resat@192.168.1.116>
2024-09-05 04:00:12 +0300

    rename chats to messages

--- src/app/messages/page.tsx
+++ src/app/messages/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import { useState } from 'react'
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
+// import { Button } from '@/components/ui/button'
+// import { Input } from '@/components/ui/input'
 import CreateMessageDialog from '@/components/CreateMessageDialog'
 
 export default function MessagesPage() {
